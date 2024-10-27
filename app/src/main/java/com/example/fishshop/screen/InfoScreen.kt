beeb297749786708858924f92bcb849ac5dd0e2c
regina <usmanova.re@yandex.ru>
2024-10-27 18:08:18 +0500

    Заготовки экранов

---
+++ app/src/main/java/com/example/fishshop/screen/InfoScreen.kt
@@ -0,0 +1,7 @@
+package com.example.fishshop.screen
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+@Composable
+fun InfoScreen() {
+    Text("Info")
+}
---
+++ app/src/main/java/com/example/fishshop/screen/ProfileScreen.kt
@@ -0,0 +1,7 @@
+package com.example.fishshop.screen
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+@Composable
+fun ProfileScreen() {
+    Text("Profile")
+}
